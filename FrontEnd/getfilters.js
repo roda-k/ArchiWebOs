9d61ce309f70b1667d8c078f967d22dcd6b6d3a6
unknown <kevin.gallau@outlook.fr>
2023-02-17 10:47:29 +0100

    login done

--- FrontEnd/getfilters.js
+++ FrontEnd/getfilters.js
@@ -1,8 +1,47 @@
 function main() {
+  const isLogged = localStorage.getItem("user_token")
+  if (isLogged)
+    adminPageView()
   fetch('http://localhost:5678/api/categories', { mode: 'cors' })
     .then(reponse => reponse.json())
     .then(reponse => getFilters(reponse))
     .catch(err => console.error(err))
+}
+
+function adminPageView() {
+  const headerRef = document.getElementById("head")
+  const loginChange = document.getElementById('loginout')
+  const modifyTrigger = document.getElementById('basePortfolio')
+  const adminNav = document.createElement('div')
+  const adminNavText = document.createElement('p')
+  const adminNavButton = document.createElement('button')
+  const modifyGalleryButton = document.createElement('button')
+
+  modifyGalleryButton.innerText = "modifier"
+  adminNavButton.innerText = "publier les changements"
+  adminNavText.innerText = "Mode édition"
+  loginChange.innerHTML = "logout"
+  adminNav.classList.add('admin-nav')
+  adminNavText.classList.add('admin-nav-text')
+  adminNavButton.classList.add('admin-nav-button')
+
+  modifyTrigger.prepend(modifyGalleryButton)
+  adminNav.appendChild(adminNavText)
+  adminNav.appendChild(adminNavButton)
+  headerRef.appendChild(adminNav)
+
+  loginChange.addEventListener("click", () => HandleLogout(), { once: true })
+
+}
+
+function HandleLogout() {
+  localStorage.removeItem("user_token")
+  window.location.reload(true)
+  window.location.replace('./index.html');
+}
+
+function HandleLogin() {
+  window.location.href = "./login.html"
 }
 
 function displayAll() {
@@ -29,7 +68,11 @@ function orderBy(filter) {
 
 function getFilters(filters) {
 
+  const loginRef = document.getElementById('loginout')
   const allButton = document.getElementById("All")
+
+  if (!localStorage.getItem("user_token"))
+    loginRef.addEventListener("click", () => HandleLogin(), { once: true })
 
   allButton.addEventListener("click", () => displayAll())
   for (let i = 0; i < filters.length; i++) {
